ab7bef264500f6c8f72fc3199fb9dafce0d7c5bf
nickopaijo <nickopaijo@gmail.com>
2022-04-11 09:13:28 +0700

    property & method

---
+++ OOP/proprerty&method.js
@@ -0,0 +1,24 @@
+// cara 1
+/*class Mail {
+    constructor() {
+        this.from = 'pengirim@dicoding.com';
+        this.contacts = [];
+        this.yourOtherProperty = 'the values';
+    }
+    sendMessage(msg, to) {
+        console.log(`you send: ${msg} to ${to} from ${this.from}`);
+        this.contacts.push(to); // menyimpan kontak baru
+    };
+}*/
+ 
+// cara 2
+/*function Mail() {
+    this.from = 'pengirim@dicoding.com';
+    this.contacts = [];
+    this.yourOtherPrototype = 'the values';
+};
+ 
+Mail.prototype.sendMessage = function (msg, to) {
+    console.log(`you send: ${msg} to  ${to} from ${this.from}`);
+    this.contacts.push(to); // menyimpan kontak baru
+};*/
