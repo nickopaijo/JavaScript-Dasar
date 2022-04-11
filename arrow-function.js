22732c36ded6bcb6fcf9874582c6a1de4994eb0f
nickopaijo <nickopaijo@gmail.com>
2022-04-11 10:58:38 +0700

    arrow

--- arrow-function.js
+++ arrow-function.js
@@ -1,11 +1,11 @@
-const ucapan = (a) => 
-{if(a == "pagi"){
-    console.log("Selamat Pagi");
-}else if(a == "siang"){
-    console.log("Selamat Siang");
-}else {
-    console.log("apaan ini?");
-}
+const ucapan = (a) => {
+    if(a == "pagi"){
+        console.log("Selamat Pagi");
+    }else if(a == "siang"){
+        console.log("Selamat Siang");
+    }else {
+        console.log("apaan ini?");
+    }
 };
 ucapan("pagi");
 
